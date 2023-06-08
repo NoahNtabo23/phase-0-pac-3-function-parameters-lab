fbda048d3d735466e326d0e9aa53e07cf279328a
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-08 23:52:11 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function introduction(name){
+    console.log(`Hi,my name is ${name}`);
+}
+introduction("Noah");
+
+function introductionWithLanguage(name,language){
+    console.log(`Hi ,my name is ${name} and i'm learning to program in ${language}`)
+}
+introductionWithLanguage("Noah","python")
+
+function introductionWithLanguageOptional(name,language="Javascript"){
+    console.log(`Hi ,my name is ${name} and i'm learning to program in ${language}`)
+}
+    
+introductionWithLanguageOptional("Tim")
+
+
+
+
+
+
+
